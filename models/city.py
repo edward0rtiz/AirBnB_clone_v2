55fa23942fe3e1ef14fb5445b2bad2bf8fa2c828
DaniVasq <danivasq50@gmail.com>
2019-12-17 02:28:45 +0000

    Updating City.py

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 class City(BaseModel):
     """This is the class for City
@@ -9,5 +10,8 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
+    __table__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     state_id = ""
     name = ""
