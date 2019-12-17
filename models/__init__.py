12f4e53fbbc0f57c2b5cbca5c930ec8f6d8559c4
edward0rtiz <931@holbertonschool.com>
2019-12-17 16:16:33 -0500

    [feature] add enviromental variables and DBStorage engine

--- models/__init__.py
+++ models/__init__.py
@@ -8,10 +8,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review"""
+
 from os import getenv
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db.storage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
