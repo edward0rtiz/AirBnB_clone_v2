72051d5e051657a341d751eb1c1a2365b40e2216
DaniVasq <danivasq50@gmail.com>
2019-12-17 02:34:55 +0000

    Updating state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
     name = ""
