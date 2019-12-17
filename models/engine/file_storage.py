935915a25a2d23df105468176a0f19db1b3d56b6
edward0rtiz <931@holbertonschool.com>
2019-12-17 16:17:50 -0500

    [fix] refactor filestorage setting all objects instanced into keys and values

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
@@ -28,6 +28,7 @@ class FileStorage:
         if cls is not None:
             if type(cls) == str:
                 cls = eval(cls)
+
             class_dict = {}
             for key, val in self.__objects.items():
                 if type(val) == cls:
