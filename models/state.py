15603f7cb833bd44e12dba7b5215f7faac5e9d9a
DaniVasq <danivasq50@gmail.com>
2019-12-18 18:19:01 +0000

    Updating changes

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the state class"""
+"""This is the state class."""
 from models.base_model import Base, BaseModel
 from models.city import City
 from sqlalchemy import Column, String
