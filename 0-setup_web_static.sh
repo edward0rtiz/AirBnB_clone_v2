cefcc8f160666428efad02c8b6dda196bf49869b
edward0rtiz <931@holbertonschool.com>
2020-01-03 23:13:28 -0500

    [fix] line addtion

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,6 +16,6 @@ echo "<!DOCTYPE html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 config='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
-sed -i "12i $config" /etc/nginx/sites-available/default
+sed -i "11i $config" /etc/nginx/sites-available/default
 service nginx restart
 exit 0
