266c25a299b1725ab74dd513e2408b09174d50cd
edward0rtiz <931@holbertonschool.com>
2020-01-03 23:25:03 -0500

    [fix] add sudo command and fist path of tmp

--- 2-deploy_web_static.py
+++ 2-deploy_web_static.py
@@ -2,7 +2,7 @@
 """ Script that distributes an archive to your web servers using do_deploy"""
 
 from fabric.api import local
-from fabric.operations import run, put
+from fabric.operations import run, put, sudo
 import os.path
 from fabric.api import env
 env.hosts = ['35.185.103.0', '35.237.21.105']
@@ -16,13 +16,13 @@ def do_deploy(archive_path):
         config = archive_path.split("/")[-1]
         ndir = ("/data/web_static/releases" + config.split(".")[0])
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(ndir))
-        run("tar -xzf {} -C {}".format(config, ndir))
-        run("rm /tmp/{}".format(config))
-        run("mv {}/web_static/ {}/".format(ndir, ndir))
-        run("rm -rf {}/web_static".format(ndir))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(ndir))
+        run("sudo mkdir -p {}".format(ndir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(config, ndir))
+        run("sudo rm /tmp/{}".format(config))
+        run("sudo mv {}/web_static/* {}/".format(ndir, ndir))
+        run("sudo rm -rf {}/web_static".format(ndir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(ndir))
         return True
     except:
         return False
